7865627ad1d6136cb70064b53a4c7aa6714018e3
Clear Code <62236986+clear-code-projects@users.noreply.github.com>
2024-05-14 15:21:14 +0100

    Add files via upload

---
+++ space shooter/code/main.py
@@ -0,0 +1,20 @@
+import pygame 
+
+# general setup 
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space shooter')
+running = True
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw the game
+    display_surface.fill('dodgerblue1')
+    pygame.display.update()
+
+pygame.quit()
