252c209af470ddd1698e7bfb4a83386daab20148
Clear Code <62236986+clear-code-projects@users.noreply.github.com>
2024-05-14 15:28:23 +0100

    Add files via upload

---
+++ Vampire survivor/code/settings.py
@@ -0,0 +1,6 @@
+import pygame 
+from os.path import join 
+from os import walk
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720 
+TILE_SIZE = 64
---
+++ Vampire survivor/data/tilesets/Objects.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Objects" tilewidth="128" tileheight="192" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="60" height="60" source="../graphics/objects/grassrock1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="56" height="60" source="../graphics/objects/grassrock2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="192" source="../graphics/objects/green_tree.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="192" source="../graphics/objects/green_tree_bushy.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="128" source="../graphics/objects/green_tree_small.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="192" source="../graphics/objects/ice_tree.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="192" source="../graphics/objects/ruin_pillar.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="104" source="../graphics/objects/ruin_pillar_broke.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="128" source="../graphics/objects/ruin_pillar_broke_alt.png"/>
+ </tile>
+ <tile id="9">
+  <image width="60" height="60" source="../graphics/objects/sandrock1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="56" height="60" source="../graphics/objects/sandrock2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="192" source="../graphics/objects/palm.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="192" source="../graphics/objects/palm_alt.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="128" source="../graphics/objects/palm_small.png"/>
+ </tile>
+</tileset>
